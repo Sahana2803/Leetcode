a4e2c9f08ea664bf26f9e1c785ac15d306982029
Sahana2803 <163704671+Sahana2803@users.noreply.github.com>
2024-08-06 07:53:43 +0530

    Time: 0 ms (100%), Space: 41.7 MB (78.29%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+       int res=0;
+      // res=nums.toremove(val); 
+      for(int i=0;i<nums.length;i++)
+      {
+        if(nums[i]!=val)
+        {
+         nums[res]=nums[i];
+         res+=1;
+        }  
+      }
+       return res;
+    }
+}
