750f15c36a896db710b05c4c36515dba394c48e2
Sahana2803 <163704671+Sahana2803@users.noreply.github.com>
2024-08-17 11:56:25 +0530

    Time: 11 ms (10.92%), Space: 44.7 MB (6.65%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String largestNumber(int[] nums)
+     {
+        String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) 
+        {
+            arr[i] = String.valueOf(nums[i]);
+        }
+        for (int i = 0; i < arr.length - 1; i++)
+         {
+            for (int j = i + 1; j < arr.length; j++)
+             {
+                String order1 = arr[i] + arr[j];
+                String order2 = arr[j] + arr[i];
+                if (order1.compareTo(order2) < 0) 
+                {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        if (arr[0].equals("0")) 
+        {
+            return "0";
+        }
+        StringBuilder largestNumber = new StringBuilder();
+        for (int i=0;i<arr.length;i++)
+        {
+            largestNumber.append(arr[i]);
+        }
+
+        return largestNumber.toString();
+    }
+}
